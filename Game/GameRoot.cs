a83c15076d0a72497a3882e27b6947b8269cf011
Jean-David Moisan <vynecompiler@gmail.com>
2023-11-30 01:41:27 -0500

    Initial project

---
+++ Game/GameRoot.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Apos.Input;
+using System.Text.Json.Serialization.Metadata;
+
+namespace GameProject {
+    public class GameRoot : Game {
+        public GameRoot() {
+            _graphics = new GraphicsDeviceManager(this) {
+                GraphicsProfile = GraphicsProfile.HiDef
+            };
+            IsMouseVisible = true;
+            Content.RootDirectory = "Content";
+
+            _settings = EnsureJson<Settings>("Settings.json", SettingsContext.Default.Settings);
+        }
+
+        protected override void Initialize() {
+            // TODO: Add your initialization logic here
+            Window.AllowUserResizing = true;
+
+            IsFixedTimeStep = _settings.IsFixedTimeStep;
+            _graphics.SynchronizeWithVerticalRetrace = _settings.IsVSync;
+
+            _settings.IsFullscreen = _settings.IsFullscreen || _settings.IsBorderless;
+
+            RestoreWindow();
+            if (_settings.IsFullscreen) {
+                ApplyFullscreenChange(false);
+            }
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent() {
+            _s = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            InputHelper.Setup(this);
+        }
+
+        protected override void UnloadContent() {
+            if (!_settings.IsFullscreen) {
+                SaveWindow();
+            }
+
+            SaveJson<Settings>("Settings.json", _settings, SettingsContext.Default.Settings);
+
+            base.UnloadContent();
+        }
+
+        protected override void Update(GameTime gameTime) {
+            InputHelper.UpdateSetup();
+
+            if (_quit.Pressed())
+                Exit();
+
+            if (_toggleFullscreen.Pressed()) {
+                ToggleFullscreen();
+            }
+            if (_toggleBorderless.Pressed()) {
+                ToggleBorderless();
+            }
+
+            // TODO: Add your update logic here
+
+            InputHelper.UpdateCleanup();
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime) {
+            GraphicsDevice.Clear(Color.Black);
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+
+        private void ToggleFullscreen() {
+            bool oldIsFullscreen = _settings.IsFullscreen;
+
+            if (_settings.IsBorderless) {
+                _settings.IsBorderless = false;
+            } else {
+                _settings.IsFullscreen = !_settings.IsFullscreen;
+            }
+
+            ApplyFullscreenChange(oldIsFullscreen);
+        }
+        private void ToggleBorderless() {
+            bool oldIsFullscreen = _settings.IsFullscreen;
+
+            _settings.IsBorderless = !_settings.IsBorderless;
+            _settings.IsFullscreen = _settings.IsBorderless;
+
+            ApplyFullscreenChange(oldIsFullscreen);
+        }
+
+        public static string GetPath(string name) => Path.Combine(AppDomain.CurrentDomain.BaseDirectory, name);
+        public static T LoadJson<T>(string name, JsonTypeInfo<T> typeInfo) where T : new() {
+            T json;
+            string jsonPath = GetPath(name);
+
+            if (File.Exists(jsonPath)) {
+                json = JsonSerializer.Deserialize<T>(File.ReadAllText(jsonPath), typeInfo);
+            } else {
+                json = new T();
+            }
+
+            return json;
+        }
+        public static void SaveJson<T>(string name, T json, JsonTypeInfo<T> typeInfo) {
+            string jsonPath = GetPath(name);
+            string jsonString = JsonSerializer.Serialize(json, typeInfo);
+            File.WriteAllText(jsonPath, jsonString);
+        }
+        public static T EnsureJson<T>(string name, JsonTypeInfo<T> typeInfo) where T : new() {
+            T json;
+            string jsonPath = GetPath(name);
+
+            if (File.Exists(jsonPath)) {
+                json = JsonSerializer.Deserialize<T>(File.ReadAllText(jsonPath), typeInfo);
+            } else {
+                json = new T();
+                string jsonString = JsonSerializer.Serialize(json, typeInfo);
+                File.WriteAllText(jsonPath, jsonString);
+            }
+
+            return json;
+        }
+
+        private void ApplyFullscreenChange(bool oldIsFullscreen) {
+            if (_settings.IsFullscreen) {
+                if (oldIsFullscreen) {
+                    ApplyHardwareMode();
+                } else {
+                    SetFullscreen();
+                }
+            } else {
+                UnsetFullscreen();
+            }
+        }
+        private void ApplyHardwareMode() {
+            _graphics.HardwareModeSwitch = !_settings.IsBorderless;
+            _graphics.ApplyChanges();
+        }
+        private void SetFullscreen() {
+            SaveWindow();
+
+            _graphics.PreferredBackBufferWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
+            _graphics.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
+            _graphics.HardwareModeSwitch = !_settings.IsBorderless;
+
+            _graphics.IsFullScreen = true;
+            _graphics.ApplyChanges();
+        }
+        private void UnsetFullscreen() {
+            _graphics.IsFullScreen = false;
+            RestoreWindow();
+        }
+        private void SaveWindow() {
+            _settings.X = Window.ClientBounds.X;
+            _settings.Y = Window.ClientBounds.Y;
+            _settings.Width = Window.ClientBounds.Width;
+            _settings.Height = Window.ClientBounds.Height;
+        }
+        private void RestoreWindow() {
+            Window.Position = new Point(_settings.X, _settings.Y);
+            _graphics.PreferredBackBufferWidth = _settings.Width;
+            _graphics.PreferredBackBufferHeight = _settings.Height;
+            _graphics.ApplyChanges();
+        }
+
+        GraphicsDeviceManager _graphics;
+        SpriteBatch _s;
+
+        Settings _settings;
+
+        ICondition _quit =
+            new AnyCondition(
+                new KeyboardCondition(Keys.Escape),
+                new GamePadCondition(GamePadButton.Back, 0)
+            );
+        ICondition _toggleFullscreen =
+            new AllCondition(
+                new KeyboardCondition(Keys.LeftAlt),
+                new KeyboardCondition(Keys.Enter)
+            );
+        ICondition _toggleBorderless = new KeyboardCondition(Keys.F11);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # LostInTheLoo
 My entry for the MonoGameJam5.
+
+## Restore
+
+```
+dotnet restore Platforms/DesktopGL
+dotnet restore Platforms/WindowsDX
+```
+
+## Run
+
+```
+dotnet run --project Platforms/DesktopGL
+dotnet run --project Platforms/WindowsDX
+```
+
+## MonoGame Content Pipeline Editor
+
+To run the MonoGame content pipeline editor, you can use the following command:
+
+```
+dotnet mgcb-editor Content/Content.mgcb
+```
+
+## Debug
+
+In vscode, you can debug by pressing F5.
+
+## Publish
+
+```
+dotnet publish Platforms/DesktopGL -c Release -r win-x64 --output artifacts/windows
+dotnet publish Platforms/DesktopGL -c Release -r osx-x64 --output artifacts/osx
+dotnet publish Platforms/DesktopGL -c Release -r linux-x64 --output artifacts/linux
+```
+
+```
+dotnet publish Platforms/WindowsDX -c Release -r win-x64 --output artifacts/windowsdx
+```
