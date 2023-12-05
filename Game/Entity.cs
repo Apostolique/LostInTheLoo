0db0f7c2243643ed9a487b2f07416cf7e53ca756
Jean-David Moisan <vynecompiler@gmail.com>
2023-12-04 19:50:32 -0500

    Fixed rendering order

--- Game/Entity.cs
+++ Game/Entity.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Threading.Tasks;
 using MonoGame.Extended;
@@ -17,5 +18,19 @@ namespace GameProject
         public RectangleF AABB;
         public float Z;
         public int Leaf;
+
+        public int CompareTo(Entity? value) {
+            if (value == null) return 1;
+            int compareZ = Z.CompareTo(value.Z);
+            return compareZ == 0 ? Leaf.CompareTo(value.Leaf) : compareZ;
+        }
+        public int GetHashCode([DisallowNull] Entity obj) {
+            return obj.Leaf.GetHashCode();
+        }
+        public bool Equals([AllowNull] Entity a, [AllowNull] Entity b) {
+            if (a == null && b == null) return true;
+            else if (a == null || b == null) return false;
+            else return a.Leaf == b.Leaf;
+        }
     }
 }
